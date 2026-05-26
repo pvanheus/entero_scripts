a935a091e3ed653cf1fb113e8210952889128163
Peter van Heusden <pvanheusden@uwc.ac.za>
2026-05-26 08:23:22 +0200

    Update tree visualisation and data for SimPlot

--- tz_enterovirus_tree_manipulation.R
+++ tz_enterovirus_tree_manipulation.R
@@ -1,47 +1,66 @@
-if (!require('pacman', character.only=TRUE)) {
-  install.packages('pacman', dependencies = TRUE)
-  library('pacman', character.only = TRUE)
+# all dependency installs now done using r2u on ubuntu
+
+#remotes::install_github("ms609/TreeDist")
+
+library(ggplot2)
+library(ape)
+library(dplyr)
+library(phangorn)
+library(treeio)
+library(tidytree)
+library(ggtree)
+library(rio)
+library(curl)
+library(remotes)
+library(TreeDist)
+library(lubridate)
+library(stringr)
+
+correct_date <- function(date_string, month="06", day="15") {
+  if (str_detect(date_string, "^\\d+$")[1]) {
+    date_string <- paste(date_string, month, sep="-")
+  }
+  if (str_detect(date_string, regex("^\\d+-\\d+$"))[1]) {
+    date_string <- paste(date_string, day, sep="-")
+  }
+  date_string
 }
 
-if (!require("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-#BiocManager::install(version = "3.18")
-BiocManager::install(c("ggtree", "treeio", "phangorn"))
-
-pacman::p_load(ggplot2, ape, dplyr,
-                 phangorn, treeio, tidytree,
-                 ggtree, rio, curl, remotes, TreeDist
-               )
-
-remotes::install_github("ms609/TreeDist")
 # load full genome and VP1 region trees
 
-vp1_tree = read.nexus("data/vp1_tree_with_dates.nex")
-vp1_tree$edge.length = vp1_tree$edge.length * 3270
-full_genome_tree = read.nexus("data/full_genome_tree.nex")
-metadata = import("data/all_microreact_metadata.csv")
+# from TZ Red Eyes 5 history
+vp1_tree_filename <- 'Galaxy39-[IQ-TREE on data 30 and data 32_ Tree labeled with dates].nex'
+vp1_tree = read.nexus(paste("data", vp1_tree_filename, sep="/"))
+metadata = import("data/all_microreact_metadata.csv") %>% 
+  mutate(Collection_Date = ymd(mapply(correct_date, Collection_Date)))
 vp1_tree_annotated = full_join(vp1_tree, metadata, join_by(label == Accession))
 
-vp1_tree_annotated$edge.length <- vp1_tree_annotated$edge.length * 3270
-full_tree <- ggtree(vp1_tree_annotated, mrsd="2024-02-01") + 
-  theme_tree2() +
-  geom_tippoint(aes(color=Country), size=2) +
-  theme_tree2()
+# vp1_tree_annotated$edge.length <- vp1_tree_annotated$edge.length * 3270
+
 
 # plot VP1 tree with clades of interest highlighted
 tree_plot <- ggtree(vp1_tree_annotated, mrsd="2024-02-01") + 
   theme_tree2() +
   geom_tippoint(aes(color=Country)) +
-  # geom_highlight(node=382, fill="blue") +
-  geom_cladelab(382, "A", offset=-35, offset.text=-18) 
+  geom_highlight(node=382, fill="blue") + # highlight and label clade A
+  geom_cladelab(382, "A", offset=-35, offset.text=-18) + 
   # geom_strip("MF419263.1", "MK989722.1", offset=-35, label="A1") +
-  # geom_highlight(node=323, fill="pink") +
-  # geom_cladelab(323, "B", offset=-34.65, offset.text=-18, extend=-2) +
-  # geom_treescale(x=2010, y=20, width=10, offset=5)
+  geom_highlight(node=323, fill="pink") + # highlight and label clade B
+  geom_cladelab(323, "B", offset=-34.65, offset.text=-18, extend=-2) +
+  geom_treescale(x=2010, y=20, width=10, offset=5) + # add scale bar
+  vexpand(0.01, direction=-1) # give extra space at bottom
 tree_plot
+
+full_genome_tree_filename <- 'Galaxy100-[IQ-TREE on data 8 and data 93_ Tree labeled with dates].nex'
+full_genome_tree = read.nexus(paste("data", full_genome_tree_filename, sep="/"))
+full_genome_tree_annotated <- full_join(full_genome_tree, metadata, join_by(label == Accession))
 
 # compute subsect of VP1 tree that matches full genome tree
 
+full_tree <- ggtree(vp1_tree_annotated, mrsd="2024-02-01") + 
+  theme_tree2() +
+  geom_tippoint(aes(color=Country), size=2) +
+  theme_tree2()
 full_genome_tips <- full_genome_tree$tip.label
 
 not_matching_tip_labels <- Filter(function(x) !(x %in% full_genome_tips), vp1_tree$tip.label)
